0ebcbe844b3636bbf7acea62cf9c011f1a6b3fc5
guli9717 <gulin9717@nextinnovation.kr>
2024-08-16 11:31:44 +0900

    added hyonde 1

--- server.js
+++ server.js
@@ -4,17 +4,36 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to serve the index.html file
+// 가입이메일_현대해상화재보험
 app.get('/page_1', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'page_1.html'));
+  res.sendFile(path.join(__dirname, 'public', 'join-hyonde.html'));
 });
 
+// 가입이메일_DB손해보험
 app.get('/page_2', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'page_2.html'));
+    res.sendFile(path.join(__dirname, 'public', 'join-db.html'));
   });
+
+  // 가입이메일_현대해상화재보험_ 펫보험
+app.get('/page_3', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'join-pet-hyonde.html'));
+});
+
+
+// 가입이메일_DB손해보험_펫보험
+app.get('/page_4', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'join-pet-db.html'));
+});
+
+// 여행자_가입증명서(1인)
+app.get('/page_5', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'single_traveler_join_hyonde.html'));
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
